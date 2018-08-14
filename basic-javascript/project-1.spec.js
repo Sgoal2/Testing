20a21affe9312d2ac2112bc97e1167d6623c9c24
CLN <cln@Sags-MacBook-Pro.local>
2018-08-14 18:26:36 -0400

    Pull,.

--- basic-javascript/project-1.spec.js
+++ basic-javascript/project-1.spec.js
@@ -1,3 +1,10 @@
 const helpers = require('./project-1');
 
 // start testing!
+it('multiply by 10', () => {
+
+    expect(helpers.multiplyByTen(1)).toEqual(10);
+    expect(helpers.multiplyByTen(10)).toEqual(100);
+    expect(helpers.multiplyByTen(25)).toEqual(250);
+
+});
